44ec4fdb2570aaec0f519654fcf136033680bee7
aaronmlin <lin58514@gmail.com>
2023-09-19 13:24:17 +0700

    update on assignment 3

--- main/models.py
+++ main/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    amount = models.IntegerField()
     price = models.IntegerField()
     description = models.TextField()
+    date_added = models.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
--- main/tests.py
+++ main/tests.py
@@ -1,6 +1,8 @@
 
 from django.test import TestCase, Client
 
+from main.models import Product
+
 class mainTest(TestCase):
     def test_main_url_is_exist(self):
         response = Client().get('/main/')
@@ -9,3 +11,17 @@ class mainTest(TestCase):
     def test_main_using_main_template(self):
         response = Client().get('/main/')
         self.assertTemplateUsed(response, 'main.html')
+
+    # def create_product(self, name = "Valk M", amount = 5, price = 30, description = "Newest model!"):
+    #     return Product.objects.create(name = name, amount = amount, price = price, description = description)
+
+    # def test_create(self):
+    #     p = self.create_product()
+    #     self.assertTrue(isinstance(p, Product))
+    #     self.assertEqual(p.__str__(), p.name)
+
+    # def test_product_name_max_length(self):
+    #     p = self.create_product()
+    #     max_length = p._meta.get_field('name').max_length
+    #     self.assertEqual(max_length, 255)
+    
