bfbd42c4d4eef92d192783874484a8388b63ea4f
aaronmlin <lin58514@gmail.com>
2023-09-27 07:09:04 +0700

    finished assignment 4

--- main/models.py
+++ main/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
     date_added = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.name
